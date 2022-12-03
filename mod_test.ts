ce66467dfc7b1746922cc69ebda2c4536e978477
v1rtl <pilll.pl22@gmail.com>
2022-12-03 23:10:30 +0200

    bump test deps

--- mod_test.ts
+++ mod_test.ts
@@ -1,5 +1,5 @@
 import { buildASTSchema, isSchema } from 'https://deno.land/x/graphql_deno@v15.0.0/mod.ts'
-import { it, expect, run } from 'https://deno.land/x/tincan@1.0.0/mod.ts'
+import { it, expect, run } from 'https://deno.land/x/tincan@1.0.1/mod.ts'
 import { gql } from './mod.ts'
 
 const typeDefs = gql`
